39e961669fd97d098af39e7ca044a936c08b528f
Rodrigo Roldan <rodrigo_roldan18@hotmail.com>
2022-09-03 01:41:42 +0200

    env variables correct

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -15,7 +15,7 @@ import commandLineArgs from 'command-line-args';
         {
             name: 'env',
             alias: 'e',
-            defaultValue: 'development',
+            defaultValue: 'production',
             type: String,
         },
     ]);
